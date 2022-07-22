b211d5300ba2ce9a5f124fa900370e041bca0baa
DogeSSBM <dogessbm@gmail.com>
2022-07-22 06:55:27 -0400

    Automatic commit.

--- ActivePlayer.h
+++ ActivePlayer.h
@@ -16,7 +16,37 @@ mCoord aBoardMpos(const uint scale)
     return (const mCoord){0};
 }
 
+void aHighlight(const Board board, const ActivePlayer active)
+{
+    if(active.mbpos.valid){
+        if(pAt(board, active.mbpos.pos).color == active.color){
+            setColor(cColor[cInv(active.color)]);
+            fillBorderCoordSquare(
+                coordMul(active.mbpos.pos, board.scale),
+                board.scale,
+                -8
+            );
+            setColor(cColor[active.color]);
+            fillBorderCoordSquare(
+                coordOffset(coordMul(active.mbpos.pos, board.scale),(const Coord){2,2}),
+                board.scale - 4,
+                -4
+            );
+        }else{
+            setColor(GREY);
+            fillBorderCoordSquare(
+                coordMul(active.mbpos.pos, board.scale),
+                board.scale,
+                -8
+            );
+        }
+    }
 
+    if(active.msrc.valid){
+        setColor(GREEN);
+        fillBorderCoordSquare(coordMul(active.msrc.pos, board.scale), board.scale, -8);
+    }
+}
 
 
 #endif /* end of include guard: ACTIVEPLAYER_H */
